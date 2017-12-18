26e62629d39705da1a5d732cb72d981f7528a1af
jbarnesrtp <direrat@gmail.com>
2017-12-18 11:38:38 -0500

    Initial project creation

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+  <!-- the router outlet, where all matched components would ber viewed -->
+  <div class="row">
+			<div class="column medium-12" style="text-align:center">
+				<img src="images/PathfinderRPGLogo_500.jpeg">
+			</div>
+		</div>
+  <router-link v-bind:to="'/'" class="button">Home</router-link>
+  
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app'
+}
+</script>
+<!-- styling for the component -->
+<style>
+
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    Welcome to the Pathfinder Portfolio
+    <div>
+			Section 15: Copyright Notice See this <a href="/ogl.html">page</a> for more information on copyright, and ogl compliance<br/>
+		  Pathfinder Roleplaying Game Databases. Copyright 2011 Mike Chopswil, d20pfsrd.com located here <a href="http://www.pathfindercommunity.net/home/databases">http://www.pathfindercommunity.net/home/databases</a>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
